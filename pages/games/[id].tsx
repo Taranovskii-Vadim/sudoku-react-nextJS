ad48ec6c6d3967581b9f7a35f0764c7bb8f0ceef
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-03-06 22:00:20 +0400

    feat(master): add change input method

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 import Button from "../../components/Button";
 
 const Game = (): JSX.Element => {
-  const dataField = [
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-  ];
+  const [dataField, onChangeDataField] = useState(() => [
+    ["5", "3", "", "", "7", "", "", "", ""],
+    ["6", "", "", "1", "9", "5", "", "", ""],
+    ["", "9", "8", "", "", "", "", "6", ""],
+    ["8", "", "", "", "6", "", "", "", "3"],
+    ["4", "", "", "8", "", "3", "", "", "1"],
+    ["7", "", "", "", "2", "", "", "", "6"],
+    ["", "6", "", "", "", "", "2", "8", ""],
+    ["", "", "", "4", "1", "9", "", "", "5"],
+    ["", "", "", "", "8", "", "", "7", "9"],
+  ]);
+
+  const onHandleChange = (y: number, x: number, value: string): void => {
+    onChangeDataField(
+      dataField.map((yItem, yIndex) => {
+        if (yIndex === y) {
+          return yItem.map((xItem, xIndex) => {
+            if (xIndex === x) {
+              return value;
+            }
+            return xItem;
+          });
+        }
+        return yItem;
+      })
+    );
+  };
 
   return (
     <div>
@@ -19,26 +37,30 @@ const Game = (): JSX.Element => {
         style={{
           display: "flex",
           flexDirection: "column",
-          // borderLeft: "1px solid black",
-          // borderTop: "1px solid black",
         }}
       >
-        {dataField.map((row, index) => {
+        {dataField.map((row, yIndex) => {
           return (
-            <div key={`${row[0]}${index}`}>
-              {row.map((item, index) => (
+            <div key={`${row[0]}${yIndex}`}>
+              {row.map((item, xIndex) => (
                 <input
-                  key={`${item}${index}`}
-                  // max={9}
-                  // type="number"
+                  max={9}
+                  value={item}
+                  disabled={!!item}
+                  key={`${item}${xIndex}`}
                   style={{
-                    width: "30px",
-                    height: "30px",
+                    width: "40px",
+                    height: "40px",
                     borderLeft: "none",
                     borderTop: "none",
+                    borderColor: "black",
+                    boxSizing: "border-box",
+                    paddingLeft: "15px",
+                    color: "black",
                   }}
-                  // disabled={!!item}
-                  // value={item}
+                  onChange={(e) =>
+                    onHandleChange(yIndex, xIndex, e.target.value)
+                  }
                 />
               ))}
             </div>
